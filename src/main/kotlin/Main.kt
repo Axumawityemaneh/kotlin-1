5656d7565b1dd8f9ad0e80acb7a860f3147b9c73
axumawityemaneh <yemanehaxumawit@gmail.com>
2024-02-07 23:55:40 +0300

    kotlin variables

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(){
+    var fullName = "Axumawit"
+    println(fullName)
+var age = 24
+    println(age)
+    var phonenumber = 254115622662
+    println(phonenumber)
+    var weightinkg = 59
+    println(weightinkg)
+val citzenkenya = false
+    println(citzenkenya)
+}
+
